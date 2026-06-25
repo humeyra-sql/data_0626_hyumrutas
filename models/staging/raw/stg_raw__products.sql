1c0ca14f64afafbee332916117c15e2a55c60d0f
Humeyra Yumrutas <humeyrayumrutas@gmail.com>
2026-06-25 09:18:56 +0000

    dosya doğru çalışıyor

--- models/staging/raw/stg_raw__products.sql
+++ models/staging/raw/stg_raw__products.sql
@@ -1,19 +1,14 @@
-with 
-
-source as (
-
-    select * from {{ source('raw', 'products') }}
-
+with source as (
+  select *
+  from {{ source('raw', 'products') }}
 ),
 
 renamed as (
-
-    select
-        products_id,
-        cast(float64(purchse_price )) as purchase_price
-
-    from source
-
+  select
+    products_id,
+    safe_cast(purchse_price as float64) as purchase_price
+  from source
 )
 
-select * from renamed
+select *
+from renamed
